648631197e8ab9e3d18ccef0afbf0814477f02a6
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2019-10-12 23:24:10 -0400

    Appjs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './actions';
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const logged = useSelector(state => state.logged);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header"> Counter {counter} </header>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+
+      <h2> {logged ? 'Secured' : ' '} </h2>
     </div>
   );
 }
