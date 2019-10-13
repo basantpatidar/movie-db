5b5143ca90033c028e7be08f8680914ede403721
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2019-10-13 18:31:50 -0400

    reduc wired

--- src/index.js
+++ src/index.js
@@ -2,8 +2,60 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import allReducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+// Pure Redux without React
+// import { createStore } from 'redux';
+
+// //Action Creator
+// const increment = () => {
+//   return {
+//     type: 'INCREMENT'
+//   };
+// };
+// const decrement = () => {
+//   return {
+//     type: 'DECREMENT'
+//   };
+// };
+
+// //Reducer
+// const counter = (state = 0, action) => {
+//   switch (action.type) {
+//     case 'INCREMENT':
+//       return state + 1;
+//     case 'DECREMENT':
+//       return state - 1;
+//     default:
+//       return state;
+//   }
+// };
+
+// //Store
+// let store = createStore(counter);
+
+// //Display it in the console
+// store.subscribe(() => console.log(store.getState()));
+
+// //Dispatch Action
+// store.dispatch(increment());
+// store.dispatch(increment());
+// store.dispatch(increment());
+// store.dispatch(decrement());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
