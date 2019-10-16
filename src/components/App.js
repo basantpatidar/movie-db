0506fbf6e8e53bbf0c2f94eb83b04b738c199348
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2019-10-15 22:49:31 -0400

    app organised

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement } from './actions';
+import { increment, decrement } from '../actions';
 import { connect } from 'react-redux';
 
 function App() {
