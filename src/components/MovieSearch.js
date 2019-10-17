a06d5271bd6bdcb22137e98e8f7a065329900e37
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2019-10-16 23:26:51 -0400

    MovieSearch component

---
+++ src/components/MovieSearch.js
@@ -0,0 +1,11 @@
+import React from 'react';
+class MovieSearch extends React.Component {
+  render() {
+    return (
+      <div>
+        <input type="text" name="MovieName"></input>
+      </div>
+    );
+  }
+}
+export default MovieSearch;
