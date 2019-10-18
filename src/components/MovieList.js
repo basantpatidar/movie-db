23305cae43894bc8082bfa144082f5d7d33ac403
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2019-10-17 23:24:07 -0400

    movie list

---
+++ src/components/MovieList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>MovieList</p>
+      </div>
+    );
+  }
+}
+export default MovieList;
