dab2bcd8d5968775741ab4ecfaa20f457a22702e
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2019-10-19 21:51:28 -0400

    combineReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import counterReducer from './counter';
 import isLoggedReducer from './isLogged';
+import searchInput from './searchInput';
 
 const allReducers = combineReducers({
   counter: counterReducer,
-  logged: isLoggedReducer
+  logged: isLoggedReducer,
+  searchInput: searchInput
 });
 
 export default allReducers;
