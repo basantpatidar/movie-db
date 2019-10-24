015d199c83580e23f717c62ca152ffa7d3f1e3d1
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2019-10-23 22:35:52 -0400

    selectedMovie

---
+++ src/components/SelectedMovie.js
@@ -0,0 +1,11 @@
+import React from 'react';
+class SelectedMovie extends React.Component {
+  render() {
+    return (
+      <div>
+        <p> SelectedMovie</p>
+      </div>
+    );
+  }
+}
+export default SelectedMovie;
