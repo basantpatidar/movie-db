0e383bc8c7941daa860e521a7ba1daa68a45abbc
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2019-10-25 13:42:08 -0400

    selectedMovie Check

--- src/components/MovieSearch.js
+++ src/components/MovieSearch.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import SelectedMovie from './SelectedMovie';
 class MovieSearch extends React.Component {
   render() {
     return (
       <div>
         <input type="text" name="MovieName"></input>
+        <SelectedMovie />
       </div>
     );
   }
