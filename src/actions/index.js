d0c170cd69b59a949dc507ce7b7648e4f5fc97f1
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2019-10-26 16:38:38 -0400

    update

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,12 +10,15 @@ import axios from 'axios';
 //   };
 // };
 
+// action creator for controlled form
 export const searchInput = term => {
   return {
     type: 'SEARCH_INPUT',
     payload: term
   };
 };
+
+//action creator for searchBar
 export const searchTerm = term => {
   axios.get('http://www.omdbapi.com/', {
     params: { s: term, apikey: process.env.development }
