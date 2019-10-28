2d9bd9de8cad2637a3b22fe2d686f6a7dedcf3b1
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2019-10-27 23:36:03 -0400

    combineReducer code clean

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,11 +4,12 @@ import isLoggedReducer from './isLogged';
 import searchInput from './searchInput';
 import searchTerm from './searchTerm';
 
+//using combineReducer
 const allReducers = combineReducers({
   counter: counterReducer,
   logged: isLoggedReducer,
-  searchInput: searchInput,
-  searchTerm: searchTerm
+  searchInput,
+  searchTerm
 });
 
 export default allReducers;
