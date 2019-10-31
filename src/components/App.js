88119a12ef29110a46d608858bf794c359a55c39
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2019-10-30 23:18:24 -0400

    cleanup

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,8 @@
 import React from 'react';
-// import { useSelector} from 'react-redux';
-// import { increment, decrement } from '../actions';
 import { connect } from 'react-redux';
 
 function App() {
-  // const counter = useSelector(state => state.counter);
-  // const logged = useSelector(state => state.logged);
-  return (
-    <div className="App">
-      {/* <header className="App-header"> Counter {counter} </header> */}
-      {/* <button onClick={() => dispatch(increment())}>+</button>
-      <button onClick={() => dispatch(decrement())}>-</button> */}
-
-      {/* <h2> {logged ? 'Secured' : ' '} </h2> */}
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
-export default App;
-//export default connect()(App);
+export default connect()(App);
