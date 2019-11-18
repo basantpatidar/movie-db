b348ef272e52a9ba3e07511c4be7e9445fe3e8eb
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2019-11-17 21:10:14 -0500

    update

--- src/components/MovieSearch.js
+++ src/components/MovieSearch.js
@@ -4,11 +4,20 @@ import * as actions from '../actions';
 import { connect } from 'react-redux';
 // import {searchInput} from '../actions'
 class MovieSearch extends React.Component {
+  state = { movieName: 'Clicked' };
+
+  handleClick = evt => this.setState({ [evt.target.name]: evt.target.value });
+
   render() {
     console.log(actions);
     return (
       <div>
-        <input type="text" name="MovieName"></input>
+        <input
+          type="text"
+          name="movieName"
+          value={this.state.movieName}
+          onChange={this.onChange}
+        ></input>
         <button>Submit</button>
         <SelectedMovie />
       </div>
@@ -24,3 +33,6 @@ export default connect(
   mapStateToProps,
   actions
 )(MovieSearch);
+
+//Controlled input
+//When clicked submit a alert box
